91453c05aa06506448b213404a559319f489b81d
Jose Ricardo Mendoza Castañeda <mjoseanakin@gmail.com>
2020-09-17 14:20:13 -0500

    Estoy Harto

---
+++ src/public/JS/banner.js
@@ -0,0 +1,16 @@
+const banner = document.getElementById('banner')
+const btnLeft = document.getElementById('btn-left')
+const btnRight = document.getElementById('btn-right')
+const pointers = document.querySelectorAll('.circle')
+const arrayPoint = []
+
+pointers.forEach((point) => {
+    arrayPoint.push(point)
+})
+
+btnRight.addEventListener('click', (e) => {
+})
+btnLeft.addEventListener('click', (e) => {
+})
+
+console.dir(arrayPoint)
