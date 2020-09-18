7e6277331efae593b0d4677aa60a0bce2ecce324
Jose Ricardo Mendoza Castañeda <mjoseanakin@gmail.com>
2020-09-18 18:37:27 -0500

    Cerramos esta branch

--- src/public/JS/banner.js
+++ src/public/JS/banner.js
@@ -1,16 +1,9 @@
 const banner = document.getElementById('banner')
 const btnLeft = document.getElementById('btn-left')
 const btnRight = document.getElementById('btn-right')
-const pointers = document.querySelectorAll('.circle')
-const arrayPoint = []
-
-pointers.forEach((point) => {
-    arrayPoint.push(point)
-})
+const pointers = document.querySelector('.circle')
 
 btnRight.addEventListener('click', (e) => {
 })
 btnLeft.addEventListener('click', (e) => {
 })
-
-console.dir(arrayPoint)
