e5b9ba98ecd948d7db3e7a4c454cbb2275c92ea3
Jose Ricardo Mendoza Castañeda <mjoseanakin@gmail.com>
2020-09-24 11:04:01 -0500

    Terminamos este Proyecto

--- src/public/JS/banner.js
+++
@@ -1,9 +0,0 @@
-const banner = document.getElementById('banner')
-const btnLeft = document.getElementById('btn-left')
-const btnRight = document.getElementById('btn-right')
-const pointers = document.querySelector('.circle')
-
-btnRight.addEventListener('click', (e) => {
-})
-btnLeft.addEventListener('click', (e) => {
-})
